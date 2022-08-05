5765873276809f5c61f72229189cf2fc429ef1bb
Amaresh <Amaresh@AMARESH-V-L>
2022-08-05 13:20:39 +0530

    ok

--- devcollege/src/main/java/com/amaresh/devcollege/serviceimplementation/CourseServiceImpl.java
+++ devcollege/src/main/java/com/amaresh/devcollege/serviceimplementation/CourseServiceImpl.java
@@ -49,50 +49,50 @@ public class CourseServiceImpl implements CourseService {
 			idData.add(enrol.getCourse_id());
 		}
 
-		boolean flag = false;
-		if (courseRepo.getStatusByCourseId(courseId).equalsIgnoreCase("Allocated")) {
-			flag = true;
-			if (flag = true) {
-				course1.setCourseName(course.getCourseName());
-				course1.setCourseDescription(course.getCourseDescription());
-				if (course.getNoOfRegistrations() > 0) {
-					course1.setNoOfRegistrations(course1.getNoOfRegistrations() + course.getNoOfRegistrations());
-				} else {
-					Map<String, String> error = new HashMap<String, String>();
-					error.put("No of Registrations Cant be Negative Value", "" + course.getNoOfRegistrations());
-					return error;
-				}
-				course1.setCourseFees(course1.getCourseFees());
-				course1.setCourseDuration(course1.getCourseDuration());
-				course1.setCourseTag(course1.getCourseTag());
-				this.courseRepo.save(course1);
-				Map<String, String> message = new HashMap<String, String>();
-				message.put("Course Name :" + " Course Discription :" + " No of Registrations :"
-						+ " Updated For Course ID =", courseId);
-				return message;
-			}
-		}
-		if (courseRepo.getStatusByCourseId(courseId) != "Allocated") {
-			course1.setCourseName(course.getCourseName());
-			course1.setCourseDescription(course.getCourseDescription());
-			course1.setCourseDuration(course.getCourseDuration());
-			course1.setCourseFees(course.getCourseFees());
-			if (course.getNoOfRegistrations() > 0) {
-				course1.setNoOfRegistrations(course1.getNoOfRegistrations() + course.getNoOfRegistrations());
-			} else {
-				Map<String, String> error = new HashMap<String, String>();
-				error.put("No of Registrations Cant be Negative Value", "" + course.getNoOfRegistrations());
-				return error;
-			}
+		if (courseRepo.getStatusByCourseId(courseId) != null) {
+			boolean flag = false;
 
-			course1.setCourseTag(course.getCourseTag());
-			this.courseRepo.save(course1);
-			Map<String, String> message = new HashMap<String, String>();
-			message.put("Successfully Updated Course details", courseId);
-			return message;
+			if (courseRepo.getStatusByCourseId(courseId).equalsIgnoreCase("Allocated")) {
+				flag = true;
+				if (flag = true) {
+					course1.setCourseName(course.getCourseName());
+					course1.setCourseDescription(course.getCourseDescription());
+					if (course.getNoOfRegistrations() > 0) {
+						course1.setNoOfRegistrations(course1.getNoOfRegistrations() + course.getNoOfRegistrations());
+					} else {
+						Map<String, String> error = new HashMap<String, String>();
+						error.put("No of Registrations Cant be Negative Value", "" + course.getNoOfRegistrations());
+						return error;
+					}
+					course1.setCourseFees(course1.getCourseFees());
+					course1.setCourseDuration(course1.getCourseDuration());
+					course1.setCourseTag(course1.getCourseTag());
+					this.courseRepo.save(course1);
+					Map<String, String> message = new HashMap<String, String>();
+					message.put("Course Name :" + " Course Discription :" + " No of Registrations :"
+							+ " Updated For Course ID =", courseId);
+					return message;
+				}
+			}
 		}
-		// }
-		return null;
+
+		course1.setCourseName(course.getCourseName());
+		course1.setCourseDescription(course.getCourseDescription());
+		course1.setCourseDuration(course.getCourseDuration());
+		course1.setCourseFees(course.getCourseFees());
+		if (course.getNoOfRegistrations() > 0) {
+			course1.setNoOfRegistrations(course1.getNoOfRegistrations() + course.getNoOfRegistrations());
+		} else {
+			Map<String, String> error = new HashMap<String, String>();
+			error.put("No of Registrations Cant be Negative Value", "" + course.getNoOfRegistrations());
+			return error;
+		}
+
+		course1.setCourseTag(course.getCourseTag());
+		this.courseRepo.save(course1);
+		Map<String, String> message = new HashMap<String, String>();
+		message.put("Successfully Updated Course details", courseId);
+		return message;
 	}
 
 	// GET ALL COURSES
@@ -128,7 +128,7 @@ public class CourseServiceImpl implements CourseService {
 				message.put("Failed to delete course details", courseId);
 				return message;
 
-			} 
+			}
 
 			if (courseRepo.getStatusByCourseId(courseId).equalsIgnoreCase("Completed")
 					|| courseRepo.getStatusByCourseId(courseId).equalsIgnoreCase("Cancelled")) {
@@ -137,14 +137,13 @@ public class CourseServiceImpl implements CourseService {
 				message1.put("Successfully Deleted Course details for ID =", courseId);
 				return message1;
 			}
-			
 
 		}
-			this.courseRepo.delete(course);
-			Map<String, String> message1 = new HashMap<String, String>();
-			message1.put("Successfully Deleted Course details for ID =", courseId);
-			return message1;
-		
-		//return null;
+		this.courseRepo.delete(course);
+		Map<String, String> message1 = new HashMap<String, String>();
+		message1.put("Successfully Deleted Course details for ID =", courseId);
+		return message1;
+
+		// return null;
 	}
 }
